06cdefa2b6d02607f4d97dd373aced83693f567f
Alexander Haegele <alexanderhaegele@t-online.de>
2022-02-01 12:06:50 +0100

    hagrid

---
+++ problems/week13-hagrid/README.md
@@ -0,0 +1,22 @@
+Tags: Recursion, Tree, Greedy
+
+Key ideas:
+* We have a tree structure
+* can do recursive traversal to get best result
+* Main challenge: finding best solution in subquadratic time at each point in recursion
+
+
+Solution:
+* recurse with three values:
+  1. max number of gold one can collected in the subtree (neglecting any kind of offset at the beginning, i.e. only take into account traversal time inside subtree)
+  2. number of nodes in subtree
+  3. full traversal time
+* we can recursively solve, for each recursive solution add 2*l to traversal time (going to and back subtree/edge) and subtract the gold we loose: (#nodes in subtree * l)
+* sort the results: r1.traversal_time * r2.num_nodes < r2.traversal_time * r1.num_nodes
+* i.e. greedily take the subtree where we loose the least number of gold!
+  * why is this correct?
+    * we have the assumption that no matter what traversal, there will always be a bit of gold
+    * i.e. we always collect nonzero gold!
+    * effectively, what we get is then #sum of all gold - #sum of gold lost
+    * --> try to minimize the right term
+* For the first few testcases, it suffices to sort after the traversal time
---
+++ problems/week13-hagrid/src/algorithm.cpp
@@ -0,0 +1,92 @@
+#include <limits>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+
+struct edge
+{
+    int v;
+    int l;
+    int traversal_time; // time it takes to go from u to all nodes under v and come back
+};
+
+struct result
+{
+    long coins;
+    long num_nodes;
+    long traversal_time;
+};
+
+std::vector<result> dp;
+
+typedef std::vector<std::vector<edge>>
+    Edges;
+
+result solve(int i, std::vector<long> &galleon, Edges &edges)
+{
+    if (edges[i].size() == 0)
+    { // leaf
+        // dp[i] = {galleon[i], 1, 0};
+        return {galleon[i], 1, 0};
+    }
+
+    std::vector<result> rec_results;
+    rec_results.reserve(edges[i].size());
+    for (auto e : edges[i])
+    {
+        result res = solve(e.v, galleon, edges);
+        // loose l coins for each node
+        rec_results.push_back({res.coins - res.num_nodes * e.l, res.num_nodes, res.traversal_time + 2 * e.l});
+    }
+
+    std::sort(rec_results.begin(), rec_results.end(), [](const result &r1, const result &r2) -> bool
+              { return r1.traversal_time * r2.num_nodes < r2.traversal_time * r1.num_nodes; });
+
+    long total_coins = galleon[i];
+    long num_nodes = 0;
+    long traversal_time = 0;
+    for(auto r : rec_results) {
+        // std::cout << i << " " << r.coins << " " <<  r.num_nodes << " " << " " << r.traversal_time << std::endl;
+        total_coins += r.coins - traversal_time * r.num_nodes;
+        num_nodes += r.num_nodes;
+        traversal_time += r.traversal_time;
+    }
+    // std::cout << "result: " << i << " " << total_coins << " " <<  num_nodes << " " << " " << traversal_time << std::endl;   
+    return {total_coins, num_nodes + 1, traversal_time} ; // return max_reward without taking current time into account
+}
+
+void testcase()
+{
+    int n;
+    std::cin >> n;
+    std::vector<long> galleon(n + 1);
+    Edges edges(n + 1);
+    dp = std::vector<result>(n + 1);
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> galleon[i + 1];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int u, v, l;
+        std::cin >> u >> v >> l;
+        edges[u].push_back({v, l, 0});
+    }
+
+    std::cout << solve(0, galleon, edges).coins << std::endl;
+    return;
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; ++i)
+        testcase();
+}
