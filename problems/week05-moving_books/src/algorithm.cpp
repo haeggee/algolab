5b38b3aefccabd4b0ff0b013cff0a573188f9601
Alexander Haegele <alexanderhaegele@t-online.de>
2022-02-10 18:17:35 +0100

    alternative version moving books

--- problems/week05-moving_books/src/algorithm.cpp
+++ problems/week05-moving_books/src/algorithm.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <set>
+
 using namespace std;
 
 void testcase() {
@@ -26,15 +28,37 @@ void testcase() {
     return;
   }
   
-  
   std::sort(strength.begin(), strength.end(), std::greater<>());
+  
+  /* master solution
+  int max_num_boxes = 0;
+  multiset<int, std::greater<>> weights_set;
+  for(int i = 0; i < m; i++) 
+    weights_set.insert(weight[i]);
+  
+  while(!weights_set.empty()) {
+    max_num_boxes++;
+    for(int i = 0; i < n; i++) {
+      auto b = weights_set.lower_bound(strength[i]);
+      if(b != weights_set.end()) {
+        weights_set.erase(b);
+      } else {
+        break;
+      }
+    }
+  }
+  */
+  
+  // My solution
   std::sort(weight.begin(), weight.end(), std::greater<>());
-  int max_num_boxes = 1;
   vector<int> num_boxes(n, 0);
+  int max_num_boxes = 1;
   num_boxes[0] = 1; // start with first guy carrying heaviest box
   for(int i = 1; i < m; i++) {
     int weight_i = weight[i];
+    // the index of the _last_ guy that is next in line to carry it
     auto next = std::upper_bound(num_boxes.begin(), num_boxes.end(), max_num_boxes, std::greater<>());
+    // the index of the first guy that is _too weak_ to carry it
     auto end = std::upper_bound(strength.begin(), strength.end(), weight_i, std::greater<>());
     int next_index = next - num_boxes.begin();
     int end_index = end - strength.begin();
